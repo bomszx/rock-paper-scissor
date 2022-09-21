5f72625458b9b66b7b4221c0a6d0370bec9e9aab
bon nocos <bon.nocos@gmail.com>
2022-09-21 23:14:47 +0800

    refactoring

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-let inputs = ['rock', 'paper', 'scissor']
-let playerScore = 0;
-let computerScore = 0;
-
-function computerPlay () {
+const computerPlay = (() => {
     let x = Math.floor(Math.random() * inputs.length);
     return inputs[x]
-}
+})()
 
 /*
 rock > scissor
@@ -23,79 +19,9 @@ use the module method, in IIFE
 
 */
 
-const player = (() => {
-    const svgIcons = document.querySelectorAll('#SVGObject')
-
-    const loopIcons = svgIcons.forEach(function(el) {
-        el.addEventListener('click',function(e) {
-            console.log('hey')
-        })    
+const clickListener = ((e) => {
+    const img = document.querySelector('.player-div');
+    img.addEventListener('click', function(e) {
+        console.log('hi')
     })
-
-    return {loopIcons}
-})();
-
-
-function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt('choose your method', '');
-    computerSelection = computerPlay()
-
-    console.log(playerSelection)
-    console.log(computerSelection)
-
-    if(playerSelection == computerSelection) {
-        console.log('It\'s a tie!')
-        playerScore, computerScore += 0;
-    } else if(playerSelection == 'rock' && computerSelection == 'scissor') {
-        console.log('Player wins, rock beats scissor')
-         playerScore += 1;
-    } else if(playerSelection == 'rock' && computerSelection == 'paper') {
-        console.log('You lose, paper beats rock')
-         computerScore += 1;
-    } else if(playerSelection == 'scissor' && computerSelection == 'paper') {
-        console.log('Player wins, scissor beats paper')
-         playerScore += 1;
-    } else if(playerSelection == 'scissor' && computerSelection == 'rock') {
-        console.log('You lose, rock beats scissor')
-         computerScore += 1
-    } else if(playerSelection == 'paper' && computerSelection == 'rock') {
-        console.log('Player wins, paper beats rock')
-         playerScore += 1;
-    } else if(playerSelection == 'paper' && computerSelection == 'scissor') {
-        console.log('You lose, scissor beats paper')
-         computerScore += 1
-    }
-    return playerScore, computerScore;
-}
-
-function game() {
-    for(let i = 0; i < 5; i++) {
-        playRound()
-        
-        console.log(playerScore, computerScore)
-        
-        if(playerScore > computerScore) {
-            console.log(`Player wins`)
-        } else if (computerScore > playerScore) {
-            console.log(`Computer wins`)
-        } else {
-            console.log(`It's a tie`)
-        }
-    }
-}
-
-  
-const removeFromArray = function(...args) {
-    const array = args[0]
-    const newArray = [];
-
-    array.forEach((item) => {
-        if(!args.includes(item)) {
-            newArray.push(item)
-        }
-    })
-    return newArray;
-};
-
-
-console.log(removeFromArray([1,2,3,4,5], 1))
+})()
