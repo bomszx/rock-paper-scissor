46c7a1201a248cd4f98d27c8645e8bc1df34cbea
bon nocos <bon.nocos@gmail.com>
2022-09-25 13:03:28 +0800

    revamped player and computer functions

--- script.js
+++ script.js
@@ -6,51 +6,39 @@ paper > rock
 no global variables
 use the module method, in IIFE
 
-1. add event listerners on the player & computer icons
-    1.1 update results icon depending on the chioce
-    1.2 add class that would higzhlight icon of choice
-    1.3 append result icon with player/computer choice
-2.
-
 */
 
-// Computer actions, should return computer choice for our playRound func
-const computer = (() => {
-    let choice;
-    const inputs = ['rock', 'paper', 'scissors']
-
-    const computerChoice = () => {
-        let x = Math.floor(Math.random() * inputs.length);
-        choice = inputs[x].toString();
-        console.log(choice, typeof(choice))
-    }
-
-    return {
-        computerChoice
-    }
-})()
-
-// Player actions, should return player choice for our playRound func, so all player actions will be here
 const player = (() => {
-    let choice;
-    const img = document.querySelector('.player-div');
+    const iconsDiv = document.querySelector('.player-div')
+    let playerChoice 
 
-    const imgListener = () => {
-        img.addEventListener('click', playerChoice)
-    }
+    iconsDiv.addEventListener('click', function(e) {
+        playerChoice = e.target.id
+        console.log(playerChoice)
+    })
+})();
 
-    const playerChoice = (e) => {
-        choice = e.target.id
-        console.log(choice)
-    }
+const computer = (() => {
 
-    return {
-        playerChoice, imgListener
-    }
-})()
+})();
 
-player.imgListener()
+const playerSelection = (() => {
+    const icons = document.querySelector('.player-div')
+    let playerInput;
 
-const playRound = (() => {
+    icons.addEventListener('click', (e) => {
+        playerInput = e.target.id
+    })
 
-})()
+    return playerInput
+})
+
+
+const getComputerChoice = (() => {
+    const inputs = ['rock', 'paper', 'scissors']
+    const x = Math.floor(Math.random() * 3)
+    let computerInput = inputs[x].toString()
+
+    return computerInput
+})
+
