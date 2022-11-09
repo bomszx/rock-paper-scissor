75011870a5e3f3f6c066d1de03b2f9ada1738ea1
bon nocos <bon.nocos@gmail.com>
2022-11-09 16:49:10 +0800

    made fn to update resutls banner into player choices

--- script.js
+++ script.js
@@ -27,33 +27,34 @@ function countdown(n){
 const computerChoice = () => {
   let arr = ['rock', 'paper', 'scissor'];
   let i = Math.floor(Math.random() * 3);
-  return arr[i]; 
+  return  arr[i]; 
 }
 
 // eventlisteners to btn that assigns the click event to the selected icon then runs the playRound fn with the selected click event target
 const playerChoice = () => {
   let pChoice;
+
+
   document.querySelectorAll('.player-icon').forEach((icon) => icon.addEventListener('click', (e) => {
     //click will call our playRound fn
+    let cChoice = computerChoice()
     pChoice = e.target.id
-    playRound(pChoice, computerChoice())
-    updateBanner()
+    playRound(pChoice, cChoice)
+    updateBanner(pChoice, cChoice)
   }))
 }
 
 const updateBanner = (playerIcon, computerIcon) => {
-  let rock = document.createElement('img')
-  rock.src = "./svg/rock.png"
-
   let player = document.getElementById('player-choice')
 
   let computer = document.getElementById('computer-choice')
 
-  player.src = "./svg/rock.png"
+  player.src = `./svg/${playerIcon}.png`
+  computer.src = `./svg/${computerIcon}.png`
 }
 
 const playRound = (getplayerChoice, getComputerChoice) => {
-  console.log('player = '+ getplayerChoice, 'computer = ' + getComputerChoice)
+  console.log(getComputerChoice)
   if(getComputerChoice == getplayerChoice) {
     console.log('same')
     return 'Same'
